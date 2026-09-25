d6f934338af4ba4db19dc7094864d2feff7252ae
SantiagoLp15 <danielsa.lopez@urosario.edu.co>
2026-09-24 22:57:43 -0500

    Retos 10

--- Corte2/Clases/Clases/18sep.py
+++ Corte2/Clases/Clases/18sep.py
@@ -9,7 +9,9 @@
 # comparo dos vecinos si estan al reves los intercambio y repito el proceso 
 # hast que nadie mas se mueva bubble sort
 
-n = [64, 25, 12, 22, 11, 90, 45, 33]
+#n = [64, 25, 12, 22, 11, 90, 45, 33]
+n = [1, 2, 3, 4, 5, 6, 7, 8]
+
 
 def bubble_sort(arr):
     comparaciones = 0
@@ -77,4 +79,48 @@ print(selection_sort(n.copy()))
 print("\n--- Insertion sort ---")
 print(insertion_sort(n.copy()))
 
+# --- MARGE Y QUICK ---
+
+def particion(arr, low, high, contadores):
+    pivote = arr[high]  # Elegimos el último elemento de la lista como pivote
+    i = low - 1         # Frontera de elementos menores al pivote
+    
+    for j in range(low, high):
+        contadores["comparaciones"] += 1
+        # Si j es menor al pivote, se hace el ntercambio 
+        if arr[j] < pivote:
+            i += 1
+            # Intercambio del elemento pequeño a la izquierda
+            arr[i], arr[j] = arr[j], arr[i]
+            contadores["intercambios"] += 1
             
+    # Colocamos el pivote en su posición correcta final
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    contadores["intercambios"] += 1
+    
+    return i + 1  # Retornamos la posición donde quedó el pivote
+
+
+# --- Quicksort ---
+def quicksort_rec(arr, low, high, contadores):
+    if low < high:
+        # pi es el índice donde el pivote ya quedó bien ubicado
+        pi = particion(arr, low, high, contadores)  
+        # Ordenamos los elementos antes y después de la partición
+        quicksort_rec(arr, low, pi - 1, contadores)
+        quicksort_rec(arr, pi + 1, high, contadores)
+
+
+# --- Función Wrapper para llamar de forma sencilla ---
+def quicksort(arr):
+    contadores = {"comparaciones": 0, "intercambios": 0}
+    quicksort_rec(arr, 0, len(arr) - 1, contadores)
+    
+    print("La cantidad de comparaciones fue:", contadores["comparaciones"])
+    print("La cantidad de intercambios fue:", contadores["intercambios"])
+    return arr
+
+
+# --- Prueba de Ejecución ---
+datos = [64, 25, 12, 22, 11, 90, 45, 33]
+print("Lista ordenada:", quicksort(datos.copy()))
